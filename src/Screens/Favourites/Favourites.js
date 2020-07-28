1f53e63e94454f597f73b06b7591b5cb088a8bdc
Billy <billyjcatchpole@gmail.com>
2020-07-28 10:11:40 +0100

    Implemented display of favourites, including re-rendering when a favourite is removed

--- src/Screens/Favourites/Favourites.js
+++ src/Screens/Favourites/Favourites.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 //css
 import classes from './Favourites.module.css'
@@ -6,13 +6,56 @@ import classes from './Favourites.module.css'
 //components
 import NavBar from "../../Shared Components/Nav_bar/Nav_bar"
 
+//redux hooks
+import { useSelector, useDispatch } from "react-redux"
+
+//redux action creators
+import { send_request } from "../../Store/Actions/0_send_request_action"
+
+//components
+import FoodItem from "../../Shared Components/Food_item/Food_item"
+
 export const Favourites = () => {
+
+    //?selectors
+    const response = useSelector(state => state.request.response)//grab the response from the api
+
+    //*states
+    const [favourites, set_favourites] = useState([])//hold the list of favourites(set by the response from the api (line37) )
+
+    //-config
+    const dispatch = useDispatch()//initialise the usedispatch hook
+
+    // //!effects
+    // eslint-disable-next-line
+    useEffect(() => { dispatch(send_request({}, "get_favourites", "get")) }, [])//only on the first render, fetch all favourites from the database to see if this item is favourites
+
+    useEffect(() => {
+
+        if (response && response.data.message === "Removed from favourites") {//if a favourite has been removed 
+
+            dispatch(send_request({}, "get_favourites", "get"))//fetch them again to display favourites without the removed one
+
+        }
+    // eslint-disable-next-line
+    }, [response])//Whenever the response changes
+
+    useEffect(() => {
+
+        if (response && response.data.message === "Favourites retrieved") {//if favourites have been retrieved
+
+            set_favourites(response.data.favourites)//set the state of favourites to the ones retreived from the database
+
+        }
+
+        // eslint-disable-next-line
+    }, [response])//re-run the effect whenever the response change
 
     return (
 
         <div className={classes.container}>
 
-            Favourito
+            {favourites.map((food_item, index) => <FoodItem data={food_item} key={index} />)}
 
             <NavBar />
 
